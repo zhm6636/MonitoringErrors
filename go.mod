8e6fbeef62246b30c925912215d0329a30152b79
理想三旬 <zhm663606998@outlook.com>
2024-02-22 19:29:53 +0800

    demo

---
+++ go.mod
@@ -0,0 +1,8 @@
+module MonitoringErrors
+
+go 1.21
+
+require (
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,60 +1,75 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"sync"
 	"time"
 )
 
 var (
-	mu           sync.Mutex
-	cond         *sync.Cond
-	monitorMap   map[string]interface{}
-	notification chan string
+	variables map[string]interface{} // 存储监控的变量
+	mu        sync.Mutex
 )
 
 func main() {
-	cond = sync.NewCond(&mu)
-	monitorMap = make(map[string]interface{})
-	notification = make(chan string)
+	variables = make(map[string]interface{})
 
-	go monitorVariable("变量1")
-	go monitorVariable("变量2")
+	// 使用context创建一个可以取消的上下文
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-	// 模拟一些操作，更新变量的值
-	go updateVariable("变量1", "值1")
-	go updateVariable("变量2", 42)
+	go monitorVariable(ctx, cancel, "例子变量")
+
+	// 模拟在一段时间后给变量赋值
+	time.Sleep(2 * time.Second)
+	setVariable("例子变量", "示例值")
+
+	// 模拟在一段时间后发生错误，取消监控goroutine
+	time.Sleep(2 * time.Second)
+	cancel()
 
 	// 主goroutine等待一段时间，然后关闭程序
-	time.Sleep(10 * time.Second)
+	time.Sleep(5 * time.Second)
 }
 
-func monitorVariable(variableName string) {
+func monitorVariable(ctx context.Context, cancel context.CancelFunc, variableName string) {
 	for {
-		mu.Lock()
-		for _, value := range monitorMap {
-			if value != nil {
-				// 发送信号，表示有值
-				notification <- variableName
-				break
-			}
-		}
-		cond.Wait() // 等待下一次监控
-		mu.Unlock()
-
-		// 处理相关逻辑
 		select {
-		case variable := <-notification:
-			fmt.Printf("变量 %s 具有值\n", variable)
-			// 处理相关逻辑
+		case <-ctx.Done():
+			// 上下文被取消，结束监控goroutine
+			fmt.Println("监控 goroutine 已取消")
+			return
+		default:
+			mu.Lock()
+			value := variables[variableName]
+			mu.Unlock()
+
+			if value != nil {
+				fmt.Printf("变量 %s 有值: %v\n", variableName, value)
+				// 在这里可以根据变量值进行相关逻辑处理
+				// 如果发现错误，取消上下文
+				if isError(value) {
+					fmt.Println("检测到错误，取消监视器 goroutine")
+					cancel()
+					return
+				}
+			}
+
+			// 模拟定时检查变量的间隔
+			time.Sleep(1 * time.Second)
 		}
 	}
 }
 
-func updateVariable(variableName string, value interface{}) {
-	time.Sleep(2 * time.Second)
+func setVariable(variableName string, value interface{}) {
 	mu.Lock()
-	monitorMap[variableName] = value
-	cond.Signal() // 通知等待的监控goroutine
+	variables[variableName] = value
 	mu.Unlock()
+}
+
+func isError(value interface{}) bool {
+	// 在这里添加检测错误的逻辑
+	// 例如，当某个特定值出现时认为是错误
+	return value == "error"
 }
